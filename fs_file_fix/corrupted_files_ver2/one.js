9e78221f9bdc90a34fa7a671da06470d6fae12ab
David C <davidthedev@users.noreply.github.com>
2018-08-17 23:26:36 +0100

    Add file duplicate remover ver 2

---
+++ fs_file_fix/corrupted_files_ver2/one.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const server = require('http').createServer();
+
+server.listen(8080);
+const fs = require('fs');
+
+const server = require('http').createServer();
+
+server.listen(8080);
---
+++ fs_file_fix/corrupted_files_ver2/two.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const server = require('http').createServer();
+
+server.listen(7000);
+const fs = require('fs');
+
+const server = require('http').createServer();
+
+server.listen(7000);
